bc1bb34b430be2b23d4fa8618602ce9ed43f62c2
seyeong Jeong <yeonian@sookmyung.ac.kr>
2025-07-09 21:22:30 +0900

    Update mirror.py

--- code/mirror.py
+++ code/mirror.py
@@ -1,37 +1,49 @@
 from ase.io import read, write
-from ase import Atoms
+from ase import Atom
 import numpy as np
-import copy
+from ase.constraints import FixAtoms
 
-def make_symmetric_adsorption(poscar_path, output_path, tol=2.5):
-    atoms = read(poscar_path, format='vasp')
+# read the POSCAR file
+atoms = read("POSCAR_111_H", format="vasp")
 
-    # Separate slab atoms and adsorbates based on z-coordinates
-    z_coords = atoms.positions[:, 2]
-    z_mid = np.mean([min(z_coords), max(z_coords)])
+# seperate slab and adsorbates
+n_pt = 36 # number of Pt atoms in the slab
+slab = atoms[:n_pt]
+adsorbates = atoms[n_pt:]
 
-    top_atoms = [atom for atom in atoms if atom.position[2] > z_mid + tol]
-    bottom_atoms = [atom for atom in atoms if atom.position[2] < z_mid - tol]
-    slab_atoms = [atom for atom in atoms if z_mid - tol <= atom.position[2] <= z_mid + tol]
+# fix the bottom of the slab
+z_positions = slab.positions[:, 2]
+z_threshold = np.median(z_positions) 
+fixed_slab_indices = [i for i, z in enumerate(z_positions) if z <= z_threshold]
 
-    # Mirror top adsorbates to bottom
-    copied_atoms = []
-    for atom in top_atoms:
-        mirrored_pos = atom.position.copy()
-        mirrored_pos[2] = 2 * z_mid - atom.position[2]
-        new_atom = copy.deepcopy(atom)
-        new_atom.position = mirrored_pos
-        copied_atoms.append(new_atom)
+# calcuate the center of mass of the adsorbates
+z_com_ads = adsorbates.get_center_of_mass()[2]
+z_top_slab = np.max(slab.positions[:, 2])
+z_bottom_slab = np.min(slab.positions[:, 2])
+dz = z_com_ads - z_top_slab
 
-    all_atoms = Atoms(
-        symbols=[atom.symbol for atom in slab_atoms + top_atoms + copied_atoms],
-        positions=np.array([atom.position for atom in slab_atoms + top_atoms + copied_atoms]),
-        cell=atoms.cell,
-        pbc=atoms.pbc
-    )
+# list to hold mirrored adsorbates
+mirrored_adsorbates = []
 
-    write(output_path, all_atoms, format='vasp')
-    print(f"{output_path}")
-    
+# generate mirrored adsorbates
+for atom in adsorbates:
+    x, y, z = atom.position
+    z_shifted = z_bottom_slab - dz + (z - z_com_ads)
+    mirrored_atom = Atom(symbol=atom.symbol, position=(x, y, z_shifted))
+    mirrored_adsorbates.append(mirrored_atom)
 
-make_symmetric_adsorption("POSCAR_100_H", "POSCAR_symmetric", tol=2.5)
+# add mirrored adsorbates to the original atoms list
+for atom in mirrored_adsorbates:
+    atoms.append(atom)
+
+# indeces for the mirrored adsorbates
+n_total = len(atoms)
+n_mirrored = len(mirrored_adsorbates)
+mirrored_indices = list(range(n_total - n_mirrored, n_total))
+
+# atom fixation
+fix_indices = fixed_slab_indices + mirrored_indices
+atoms.set_constraint(FixAtoms(indices=fix_indices))
+
+# save the new structure to a POSCAR file
+write("111H_POSCAR_sym", atoms, format="vasp", direct=False)
